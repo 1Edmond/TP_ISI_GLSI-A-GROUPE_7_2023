27bb22421ae7e293bbada608521d5e41f0ef274d
1Edmond <friedocode@gmail.com>
2023-04-13 19:24:30 +0000

    Début du projet.

---
+++ siexpose/src/test/java/springtpiai/siexpose/SiexposeApplicationTests.java
@@ -0,0 +1,13 @@
+package springtpiai.siexpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiexposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
