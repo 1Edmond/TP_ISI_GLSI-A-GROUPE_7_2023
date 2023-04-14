08ede4e75af4ba6089eb6722998c4fdaed2ff9dc
arnaud1999 <arnauddjana62@gmail.com>
2023-04-14 10:03:27 +0000

    Message de votre commit

---
+++ siexpose/src/main/java/springtpiai/siexpose/Client.java
@@ -0,0 +1,31 @@
+package springtpiai.siexpose.models;
+
+import jakarta.persistence.*;
+import springtpiai.siexpose.allEnums.Sexe;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "Clients")
+public class Client {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String nom, prenom, adresse, telephone, mail, nationalite;
+    Sexe sexe;
+    LocalDate dateNaissance;
+
+    @OneToMany(targetEntity = Compte.class,cascade = CascadeType.ALL)
+    @JoinColumn(name ="idClient",referencedColumnName = "id")
+    List<Compte> Comptes;
+
+}
